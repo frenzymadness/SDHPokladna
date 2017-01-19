b7e7146fad5c7865b05912af1399f67bb95e6399
Lumir Balhar <lbalhar@redhat.com>
2017-01-19 22:37:43 +0100

    Working prototype

--- app.js
+++ app.js
@@ -4,5 +4,17 @@ app.controller('ItemListController', function ItemListController($scope, $http) 
   $http.get('data.json')
        .then(function(res){
           $scope.items = res.data;
+          $scope.reset();
         });
+
+  $scope.changePrice = function changePrice(price) {
+    $scope.totalPrice += price;
+  };
+
+  $scope.reset = function reset() {
+    $scope.totalPrice = 0;
+    angular.forEach($scope.items, function(value, key) {
+        this.ordered = false;
+    });
+  };
 });
