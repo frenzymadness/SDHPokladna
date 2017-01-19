eb2a91a638a5e28ee826b34aa47b5b2ca6367da8
Lumir Balhar <lbalhar@redhat.com>
2017-01-19 21:58:07 +0100

    Get rid of Vue and use angular instead.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+var app = angular.module('App', [])
+
+app.controller('ItemListController', function ItemListController($scope, $http) {
+  $http.get('data.json')
+       .then(function(res){
+          $scope.items = res.data;
+        });
+});
