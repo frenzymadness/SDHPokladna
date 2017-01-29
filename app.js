2cbb723431391e53f54d582c1ffd63dc11c54b7e
Lumir Balhar <lbalhar@redhat.com>
2017-01-29 14:52:05 +0100

    Added text of order and improved functionality.

--- app.js
+++ app.js
@@ -7,14 +7,47 @@ app.controller('ItemListController', function ItemListController($scope, $http) 
           $scope.reset();
         });
 
-  $scope.changePrice = function changePrice(price) {
-    $scope.totalPrice += price;
+  $scope.addItem = function addItem(item, type) {
+    if (type == "small") {
+      $scope.totalPrice += item.smallPrice;
+      item.orderedSmall += 1;
+    } else {
+      $scope.totalPrice += item.price;
+      item.ordered += 1;
+    }
+    $scope.regenerateTextOrder();
+  };
+
+  $scope.removeItem = function removeItem(item, type) {
+    if (type == "small") {
+      $scope.totalPrice -= item.smallPrice;
+      item.orderedSmall -= 1;
+    } else {
+      $scope.totalPrice -= item.price;
+      item.ordered -= 1;
+    }
+    $scope.regenerateTextOrder();
+  };
+
+  $scope.regenerateTextOrder = function regenerateTextOrder() {
+    $scope.order = "";
+    angular.forEach($scope.items, function(item, key) {
+      if (item.ordered > 0) {
+        $scope.order = $scope.order + "Velka " + item.name + " " + item.ordered + " ks\n";
+      }
+      if (item.orderedSmall > 0) {
+        $scope.order = $scope.order + "Mala " + item.name + " " + item.orderedSmall + " ks\n";
+      }
+    });
   };
 
   $scope.reset = function reset() {
     $scope.totalPrice = 0;
-    angular.forEach($scope.items, function(value, key) {
-        this.ordered = false;
+    $scope.order = "";
+    
+    angular.forEach($scope.items, function(item, key) {
+        item.ordered = 0;
+        item.orderedSmall = 0;
     });
   };
 });
